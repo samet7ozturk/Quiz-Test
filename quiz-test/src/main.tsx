c4dc72cf064b710a9517e8ce2bc4ffccb59e24b9
samet7ozturk <sametozturk973@gmail.com>
2024-01-30 21:58:09 +0300

    Provider wrap

--- quiz-test/src/main.tsx
+++ quiz-test/src/main.tsx
@@ -1,10 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import store from "./store/store";
+import App from "./App";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
+const root = ReactDOM.createRoot(document.getElementById("root")!);
+root.render(
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
-)
+  </Provider>
+);
