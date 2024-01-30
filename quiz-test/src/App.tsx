7f1e1563b57493d5b4983bde7964e360b68efec3
samet7ozturk <sametozturk973@gmail.com>
2024-01-30 22:13:05 +0300

    First question render

--- quiz-test/src/App.tsx
+++ quiz-test/src/App.tsx
@@ -1,33 +1,30 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import { useAppSelector } from "./store/store";
+import { RootState } from "./store/store";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const questionState = useAppSelector((state: RootState) => state.question);
+  console.log(questionState);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <nav>
+        <p>Username:</p>
+        <p>Score:</p>
+        <p>Rank:</p>
+        <p>Settings</p>
+      </nav>
+      <main>
+        <h1>QUIZ</h1>
+        <p>Question 1</p>
+        <p>{questionState.questions[0].text}</p>
+        <div>
+          {questionState.questions[0].options.map((item) => (
+            <p key={item.id}>{item.text}</p>
+          ))}
+        </div>
+      </main>
+
+      <p></p>
     </>
   );
 }
