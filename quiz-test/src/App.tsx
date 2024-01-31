14851c9067f04c0e34795b453a5b1e4d3693a2be
samet7ozturk <sametozturk973@gmail.com>
2024-01-31 10:21:05 +0300

    CSS edits

--- quiz-test/src/App.tsx
+++ quiz-test/src/App.tsx
@@ -29,26 +29,49 @@ function App() {
 
   return (
     <>
-      <nav>
-        <p>Username:</p>
-        <p>Score:{score}</p>
-        <p>Rank:</p>
-        <p>Settings</p>
-        <select>
-          <option value="turkish">Türkçe</option>
-          <option value="english">İngilizce</option>
-        </select>
-        <button onClick={switchTheme}>
-          {click1 ? "dark mode" : "light mode"}
-        </button>
+      <nav
+        className={`flex justify-between items-center h-16 ${
+          click1 ? "bg-[#22092C]" : "bg-[#87C4FF]"
+        } px-6`}
+      >
+        <div className="flex gap-4 text-white">
+          <p>Username:</p>
+          <p>Score:{score}</p>
+          <p>Rank:</p>
+        </div>
+
+        <div className="flex gap-4">
+          <select>
+            <option value="turkish">Türkçe</option>
+            <option value="english">İngilizce</option>
+          </select>
+          <button onClick={switchTheme}>
+            {click1 ? "light mode" : "dark mode"}
+          </button>
+        </div>
       </nav>
-      <main>
-        <h1>QUIZ</h1>
-        <p>Question {`${count}`}</p>
-        <p>{questionState.questions[`${count}`].text}</p>
-        <div>
+
+      <main
+        className={`${
+          click1 ? "bg-[#872341]" : "bg-[#E0F4FF]"
+        } h-[calc(100vh_-_64px)]`}
+      >
+        <div className="flex justify-center pt-20">
+          <h1 className="text-3xl">QUIZ</h1>
+        </div>
+
+        <div className="flex justify-center py-8">
+          <p className="text-2xl">Question {`${count}`}</p>
+        </div>
+        <div className="flex justify-center pb-8">
+          <p className="text-2xl">{questionState.questions[`${count}`].text}</p>
+        </div>
+        <div className="flex flex-col gap-4 items-center">
           {questionState.questions[0].options.map((item) => (
             <button
+              className={`border-2 rounded-lg w-[200px] h-[40px] hover:scale-105 transition ${
+                click1 ? "bg-[#F05941]" : "bg-[#39A7FF]"
+              } text-white text-xl`}
               key={item.id}
               onClick={() => handleOptionClick(item.correct)}
             >
@@ -57,8 +80,6 @@ function App() {
           ))}
         </div>
       </main>
-
-      <p></p>
     </>
   );
 }
