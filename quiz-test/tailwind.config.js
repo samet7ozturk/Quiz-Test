68b9dd8528549dc49ef7ddce6ddc59a601a04c6e
samet7ozturk <sametozturk973@gmail.com>
2024-02-01 13:38:21 +0300

    Animations create

--- quiz-test/tailwind.config.js
+++ quiz-test/tailwind.config.js
@@ -5,11 +5,21 @@ export default {
     extend: {
       animation: {
         animation1: "animation1 0.3s linear both",
+        animation2: "animation2 0.4s linear both",
+        animation3: "animation3 0.4s linear both",
       },
       keyframes: {
         animation1: {
           "0%": { transform: "scale(0.5)", opacity: 0.5 },
           "100%": { transform: "scale(1)", opacity: 1 },
+        },
+        animation2: {
+          "0%": { transform: "translateX(-100%)", opacity: 0 },
+          "100%": { transform: "translateX(0)", opacity: 1 },
+        },
+        animation3: {
+          "0%": { transform: "translateY(-100%)", opacity: 0.5 },
+          "100%": { transform: "translateY(0)", opacity: 1 },
         },
       },
       fontFamily: {
